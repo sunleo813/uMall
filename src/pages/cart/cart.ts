c093183f24d6265bbe0f4120e59466b0521a7781
sunleo813 <sunleo@livemail.tw>
2018-05-04 14:18:10 +0800

    Modify tab pages

---
+++ src/pages/cart/cart.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-contact',
+  templateUrl: 'cart.html'
+})
+export class CartPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
