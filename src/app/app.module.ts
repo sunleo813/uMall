b1d9db60ecd4b8ed826d2e2407da871a4dd0d77d
Lou I <louislu@Lous-MacBook-Air.local>
2018-05-05 11:53:06 +0800

    rebuild tabs from Mac

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { CategoryPage } from '../pages/category/category';
+import { CartPage } from '../pages/cart/cart';
+import { HomePage } from '../pages/home/home';
+import { UserPage } from '../pages/user/user';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -14,10 +15,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    TabsPage,
+    CategoryPage,
+    CartPage,
     HomePage,
-    TabsPage
+    UserPage
   ],
   imports: [
     BrowserModule,
@@ -26,10 +28,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    TabsPage,
+    CategoryPage,
+    CartPage,
     HomePage,
-    TabsPage
+    UserPage
   ],
   providers: [
     StatusBar,
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
+import { CategoryPage } from '../category/category';
+import { CartPage } from '../cart/cart';
 import { HomePage } from '../home/home';
+import { UserPage } from '../user/user';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -10,8 +11,9 @@ import { HomePage } from '../home/home';
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab2Root = CategoryPage;
+  tab3Root = CartPage;
+  tab4Root = UserPage;
 
   constructor() {
 
